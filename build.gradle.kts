436f10a09dc57e0ee9aafb0b468ad10c306cf319
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-06-13 18:14:40 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     modImplementation("net.axay:fabrikmc-nbt:1.8.1")
     modImplementation("net.axay:fabrikmc-persistence:1.8.1")
     modImplementation("net.axay:fabrikmc-game:1.8.1")
+    modImplementation("net.axay:fabrikmc-network:1.8.1")
 }
 
 tasks.test {
