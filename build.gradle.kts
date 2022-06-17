b326229d61b48743ff34a6634d9765e007507001
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-06-17 18:00:23 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
         addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19+build.1:v2"))
         officialMojangMappings()
     })
-    modImplementation("net.fabricmc:fabric-loader:0.14.7")
+    modImplementation("net.fabricmc:fabric-loader:0.14.8")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.55.3+1.19")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.8.0+kotlin.1.7.0")
     modImplementation("net.axay:fabrikmc-core:1.8.1")
