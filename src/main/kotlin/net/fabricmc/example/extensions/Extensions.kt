8cdaf8178cb1bbf6bf78f85683f39c481611a16c
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-06-20 20:19:41 +0200

    Update Extensions.kt

--- src/main/kotlin/net/fabricmc/example/extensions/Extensions.kt
+++ src/main/kotlin/net/fabricmc/example/extensions/Extensions.kt
@@ -1,6 +1,7 @@
 package net.fabricmc.example.extensions
 
 import net.axay.fabrik.core.Fabrik
+import net.axay.fabrik.core.text.sendText
 import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
@@ -11,4 +12,16 @@ fun world(world_name: String): ServerLevel {
     val resourceKey: ResourceKey<Level> = ResourceKey.create(Registry.DIMENSION_REGISTRY, ResourceLocation(world_name))
     val world: ServerLevel? = Fabrik.currentServer!!.getLevel(resourceKey)
     return world!!
+}
+
+fun broadcast(text: String, color: Int = 0xffffff, italic: Boolean = false, bold: Boolean = false) {
+    Fabrik.currentServer?.playerList?.players?.forEach {
+        it.sendText {
+            text(text) {
+                this.color = color
+                this.italic = italic
+                this.bold = bold
+            }
+        }
+    }
 }
