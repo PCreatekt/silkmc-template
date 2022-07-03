781ee4cca64d85dce360aea6d6c0884c875eb09b
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-07-03 11:16:59 +0200

    Update Extensions.kt

--- src/main/kotlin/net/fabricmc/example/extensions/Extensions.kt
+++ src/main/kotlin/net/fabricmc/example/extensions/Extensions.kt
@@ -1,19 +1,22 @@
 package net.fabricmc.example.extensions
 
 import net.silkmc.silk.core.Silk
-import net.silkmc.silk.core.text.sendText
 import net.minecraft.core.Registry
+import net.minecraft.network.chat.ChatType
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.Level
+import net.silkmc.silk.core.annotations.DelicateSilkApi
+import net.silkmc.silk.core.text.literalText
 
 fun world(world_name: String): ServerLevel {
     val resourceKey: ResourceKey<Level> = ResourceKey.create(Registry.DIMENSION_REGISTRY, ResourceLocation(world_name))
-    val world: ServerLevel? = Fabrik.currentServer!!.getLevel(resourceKey)
+    val world: ServerLevel? = Silk.currentServer!!.getLevel(resourceKey)
     return world!!
 }
 
+@OptIn(DelicateSilkApi::class)
 fun broadcast(text: String, color: Int = 0xffffff, italic: Boolean = false, bold: Boolean = false) {
     Silk.currentServer?.playerList?.broadcastSystemMessage(literalText(text) {
             this.color = color
