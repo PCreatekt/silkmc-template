f4a323f781e640d8b4bb0b2cff1221153efee1be
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-09-15 17:21:28 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val silkVersion = "1.9.2"
+
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("fabric-loom") version "0.12-SNAPSHOT"
     id("io.github.juuxel.loom-quiltflower") version "1.7.3"
-    id("org.quiltmc.quilt-mappings-on-loom") version "4.2.0"
+    id("org.quiltmc.quilt-mappings-on-loom") version "4.2.1"
 }
 
 group = "net.fabricmc.example"
@@ -15,21 +17,21 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19")
+    minecraft("com.mojang:minecraft:1.19.2")
     mappings(loom.layered {
-        addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19+build.1:v2"))
+        addLayer(quiltMappings.mappings("org.quiltmc:quilt-mappings:1.19.2+build.14:v2"))
         officialMojangMappings()
     })
-    modImplementation("net.fabricmc:fabric-loader:0.14.8")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.56.3+1.19")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.0+kotlin.1.7.0")
-    modImplementation("net.silkmc:silk-core:1.9.0")
-    modImplementation("net.silkmc:silk-commands:1.9.0")
-    modImplementation("net.silkmc:silk-igui:1.9.0")
-    modImplementation("net.silkmc:silk-nbt:1.9.0")
-    modImplementation("net.silkmc:silk-persistence:1.9.0")
-    modImplementation("net.silkmc:silk-game:1.9.0")
-    modImplementation("net.silkmc:silk-network:1.9.0")
+    modImplementation("net.fabricmc:fabric-loader:0.14.9")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.61.0+1.19.2")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.3+kotlin.1.7.10")
+    modImplementation("net.silkmc:silk-core:$silkVersion")
+    modImplementation("net.silkmc:silk-commands:$silkVersion")
+    modImplementation("net.silkmc:silk-igui:$silkVersion")
+    modImplementation("net.silkmc:silk-nbt:$silkVersion")
+    modImplementation("net.silkmc:silk-persistence:$silkVersion")
+    modImplementation("net.silkmc:silk-game:$silkVersion")
+    modImplementation("net.silkmc:silk-network:$silkVersion")
 }
 
 tasks.test {
@@ -38,8 +40,10 @@ tasks.test {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs += "-Xskip-prerelease-check"
+        }    }
     withType<JavaCompile> {
         options.release.set(17)
     }
